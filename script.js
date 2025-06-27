81de8c43df3b54a9d87fe115668a27e8de579ad4
Levi1724 <ruelascallalevi@gmail.com>
2025-06-26 19:11:47 -0500

    Delete script.js
    Fin de mlno

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleDailyBook(exerciseId) {
-    const dailyBookContainer = document.getElementById(exerciseId);
-    if (dailyBookContainer.classList.contains('hidden')) {
-        dailyBookContainer.classList.remove('hidden');
-    } else {
-        dailyBookContainer.classList.add('hidden');
-    }
-}
